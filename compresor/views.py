aba3dfe2c27a51b7c1d47a04a9681a05bdd3571a
Israel Colín <israaacolin@gmail.com>
2023-08-22 21:49:22 -0600

    Insertando comentarios a el archivo views.py

--- compresor/views.py
+++ compresor/views.py
@@ -13,14 +13,18 @@ def list_files(request):
     return render(request, 'lista.html', {'files': files})
 
 def compress_files(request):
+    # Archivos recibidos del formulario
     selected_files = request.POST.getlist('selected_files')
+    # Nombre del archivo como se guardará
     zip_filename = "compressed_files.zip"
 
+    # Proceso de compresión de archivos
     with zipfile.ZipFile(zip_filename, 'w') as zipf:
         for file in selected_files:
             file_path = os.path.join("C:/Users/siste/OneDrive/Escritorio/Excel", file)
             zipf.write(file_path, os.path.basename(file_path))
 
+    # Respuesta de descarga de los archivos
     response = FileResponse(open(zip_filename, 'rb'))
     response['Content-Disposition'] = f'attachment; filename="{zip_filename}"'
 

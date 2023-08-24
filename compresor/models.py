71e80a1c222ddbc474b9d7eb37dd89e3571268ee
Israel Colín <israaacolin@gmail.com>
2023-08-23 19:31:12 -0600

    Eliminando archivos innecesarios

--- compresor/models.py
+++ compresor/models.py
@@ -1,8 +1,3 @@
 from django.db import models
 
 # Create your models here.
-class Usuario(models.Model):
-    id = models.AutoField(primary_key=True)
-
-    def __str__(self) -> str:
-        return self.id

62fec8d37d01120539400551b7b5ec23210bf7a3
Israel Colín <israaacolin@gmail.com>
2023-12-05 21:40:01 -0600

    Iteración sobre base de datos para mostrar facturas

--- compresor/models.py
+++ compresor/models.py
@@ -11,4 +11,3 @@ class Facturas(models.Model):
     FechaDeTimbrado = models.CharField(_("Fecha de timbrado"), max_length=500)
     RutaProduccion = models.CharField(_("Ruta Producción"), max_length=500)
     RutaAppFact = models.CharField(_("RutaAppFact"), max_length=500)
-
--- compresor/views.py
+++ compresor/views.py
@@ -2,26 +2,44 @@ import os
 import zipfile
 from django.http import FileResponse
 from django.shortcuts import render, HttpResponse
+from .models import *
 
 
 def list_files(request):
-    files = None
     mensaje = None
+    archivos = None
     # Si el formulario manda datos por POST
     if request.POST:
+        archivos = []
         # RFC ingresado por el usuario
         rfc = request.POST.get('rfc')
-        # Ruta de la carpeta de documentos
-        folder_path = "C:/Users/siste/OneDrive/Escritorio/Excel"
-        # Obtener una lista de archivos en la carpeta
-        all_files = os.listdir(folder_path)
-        # Filtrar solo los archivos que tengan el nombre del campo RFC
-        files = [file for file in all_files if file.startswith(rfc)]
-        # Si no se encuentran archivos, manda mensaje de error
-        if len(files) == 0:
-            mensaje = "No se encontraron facturas para este RFC"
+        # Resultado de busqueda en BD
+        rutas_facturas = Facturas.objects.filter(rfc=rfc)
+        # Si rutas_facturas == 0, mandar alerta de que no existen facturas
+        if len(rutas_facturas) == 0:
+            mensaje = "No existen facturas para este RFC"
+        # Iterar sobre rutas
+        for ruta_factura in rutas_facturas:
+            # Agregar extenciones a archivos
+            ruta_pdf = ruta_factura.RutaAppFact + '.pdf'
+            ruta_xml = ruta_factura.RutaAppFact + '.xml'
+            # Solo el nombre de los archivos
+            nombre_archivo_pdf = os.path.basename(ruta_pdf)
+            nombre_archivo_xml = os.path.basename(ruta_xml)
+            # Intento para agregar archivos a lista
+            try:
+                # Si la ruta completa pertenece a un archivo agragarlo a lista
+                if os.path.isfile(ruta_pdf):
+                    archivos.append(nombre_archivo_pdf)
+                    archivos.append(nombre_archivo_xml)
+                # Si la ruta no pertenece a un archivo, mandar alerta
+                else:
+                    mensaje = f"El archivo '{nombre_archivo_pdf}' no existe en la ruta '{ruta_pdf}'"
+                # Manejo de errores
+            except OSError as e:
+                mensaje = f"No se pudieron listar los archivos en la ruta '{ruta_pdf}': {str(e)}"
     # Retorna template, y si tiene archivos los manda también
-    return render(request, 'lista.html', {'files': files, 'mensaje': mensaje})
+    return render(request, 'lista.html', {'files': archivos, 'mensaje': mensaje})
 
 
 def compress_files(request):
@@ -35,9 +53,8 @@ def compress_files(request):
         with zipfile.ZipFile(zip_filename, 'w') as zipf:
             for file in selected_files:
                 file_path = os.path.join(
-                    "C:/Users/siste/OneDrive/Escritorio/Excel", file)
+                    r'E:\AIFA\JAI040322QI\202311\01', file)
                 zipf.write(file_path, os.path.basename(file_path))
-
         # Respuesta de descarga de los archivos
         response = FileResponse(open(zip_filename, 'rb'))
         response['Content-Disposition'] = f'attachment; filename="{zip_filename}"'
